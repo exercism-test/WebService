73bef8a37e4002e2c9669d72096c170190719404
Tom van Hateren <tom112112@gmail.com>
2018-04-03 20:04:13 +0200

    Call all the methods of the Calculator

--- client/src/CalculatorClient.java
+++ client/src/CalculatorClient.java
@@ -3,6 +3,10 @@ import calculator.CalculatorService;
 public class CalculatorClient {
 	public static void main (String[] argv) {
 		calculator.Calculator service = new CalculatorService().getCalculatorPort();
-		System.out.printf("2 + 3 = %d", service.add(2, 3));
+
+		System.out.printf("2 + 3 = %d\n", service.add(2, 3));
+		System.out.printf("2 - 3 = %d\n", service.sub(2, 3));
+		System.out.printf("2 * 3 = %d\n", service.mul(2, 3));
+		System.out.printf("2 / 3 = %d\n", service.div(2, 3));
 	}
 }
